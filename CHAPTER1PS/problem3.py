f253f2f7f8840c6870a4db2ebbe050d6303b924d
Tirth-Bhatta <tirthbhatta0@gmail.com>
2026-06-17 17:31:58 +0545

    remove certain words

--- CHAPTER1PS/problem3.py
+++ CHAPTER1PS/problem3.py
@@ -4,5 +4,5 @@ engine = pyttsx3.init()
 # For Mac, If you face error related to "pyobjc" when running the `init()` method :
 # Install 9.0.1 version of pyobjc : "pip install pyobjc>=9.0.1"
 
-engine.say("shut up bitch")
+engine.say("shut up")
 engine.runAndWait()
