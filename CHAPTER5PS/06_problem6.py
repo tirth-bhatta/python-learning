8f845e71aa8e1bac7aca43cede27250a8a565c01
Tirth-Bhatta <tirthbhatta0@gmail.com>
2026-06-17 23:01:00 +0545

    Create dictionary of friends and their favorite languages

---
+++ CHAPTER5PS/06_problem6.py
@@ -0,0 +1,19 @@
+d = {}
+
+name = input("Enter friend's name: ")
+lang = input("Enter language name: ")
+d.update({name: lang})
+
+name = input("Enter friend's name: ")
+lang = input("Enter language name: ")
+d.update({name: lang})
+
+name = input("Enter friend's name: ")
+lang = input("Enter language name: ")
+d.update({name: lang})
+
+name = input("Enter friend's name: ")
+lang = input("Enter language name: ")
+d.update({name: lang})
+
+print(d)
