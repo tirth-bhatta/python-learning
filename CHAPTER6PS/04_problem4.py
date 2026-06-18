c84d9913e8338a9e705bd210b25986bbef7bf233
Tirth-Bhatta <tirthbhatta0@gmail.com>
2026-06-18 14:33:35 +0545

    Add username character count check

---
+++ CHAPTER6PS/04_problem4.py
@@ -0,0 +1,7 @@
+username = input("Enter your username: ")
+
+if (len(username)<10):
+    print("this username contin less than 10 characters")
+
+else:
+    print("this username contains 10 or more than 10 characters")
