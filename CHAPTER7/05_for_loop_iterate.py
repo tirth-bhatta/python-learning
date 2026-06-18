7c02099ebcceaa4c733aabb25699501b50d93c2d
Tirth-Bhatta <tirthbhatta0@gmail.com>
2026-06-18 16:47:24 +0545

    Practice for loops with different data types

---
+++ CHAPTER7/05_for_loop_iterate.py
@@ -0,0 +1,15 @@
+# for loop with lists
+l = [1, 4, 6, 234, 6, 764]
+for i in l:
+    print(i)
+
+# for loop with tuples
+t = (6, 231, 75, 122)
+for i in t:
+    print(i)
+
+# for loop with tuples
+
+s = "Harry"
+for i in s:
+    print(i)
