c1bdccb335e2f3351d6e92e821b3759dcdd848ed
Tirth-Bhatta <tirthbhatta0@gmail.com>
2026-06-18 14:07:42 +0545

    Add student pass/fail check using percentage conditions

---
+++ CHAPTER6PS/02_problem2.py
@@ -0,0 +1,13 @@
+marks1 = int(input("Enter Marks 1: "))
+marks2 = int(input("Enter Marks 2: "))
+marks3 = int(input("Enter Marks 3: "))
+
+# checks for total percentage
+
+total_percentage = (100*(marks1 + marks2 + marks3))/300
+
+if(total_percentage>=40 and marks1>33 and marks2>33 and marks3>33):
+    print("You passed", total_percentage)
+
+else:
+    print("You failed, try agin next year", total_percentage)
