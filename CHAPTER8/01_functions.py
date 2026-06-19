840cf4dae7b21395f1566791c42a8bb7551aaa8c
Tirth-Bhatta <tirthbhatta0@gmail.com>
2026-06-19 19:58:04 +0545

    Add average calculation function

---
+++ CHAPTER8/01_functions.py
@@ -0,0 +1,36 @@
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
+
+
+# average = (a + b + c)/3
+# print(average)
+
+
+# a = int(input("Enter your number: "))
+# b = int(input("Enter your number: "))
+# c = int(input("Enter your number: "))
+
+
+# average = (a + b + c)/3
+# print(average)
+
+# Function definition
+
+def avg():
+    a = int(input("Enter your number: "))
+    b = int(input("Enter your number: "))
+    c = int(input("Enter your number: "))
+
+
+    average = (a + b + c)/3
+    print(average)
+
+avg() #function call
+print("thank you")
+avg()
+print("thank you")
+avg()
+print("thank you")
+avg()
+avg()
