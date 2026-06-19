ddb52049434562f45ab5e595ae53bb5f4ee3307f
Tirth-Bhatta <tirthbhatta0@gmail.com>
2026-06-19 21:14:57 +0545

    Add factorial calculation using recursion

---
+++ CHAPTER8/05_recursion.py
@@ -0,0 +1,20 @@
+'''
+
+factorial(1) =  1
+factorial(2) =  2 X 1
+factorial(3) =  3 X 2 X 1
+factorial(4) =  4 X 3 X 2 X 1
+factorial(5) =  5 X 4 X 3 X 2 X 1
+
+factorial(n) =  n X n -1 X ..... 3 X 2 X 1
+
+factorial = n * factorial(n-1)
+'''
+
+def factorial(n):
+    if(n==0 or n==1):
+        return 1
+    return n* factorial(n-1)
+
+n = int(input("Enter a number: "))
+print(f"the factorial of this number is:{factorial(n)}")
