95942d414fb7af4ae14d5f0110cb4f70d4f1083e
Tirth-Bhatta <tirthbhatta0@gmail.com>
2026-06-24 10:19:27 +0545

    Build class-based model for storing programmer details at Microsoft

---
+++ CHAPTER10PS/01_problem1.py
@@ -0,0 +1,13 @@
+class programmer:
+    company = "Microsoft"
+    def __init__(self, name, salary, pin):
+        self.name = name
+        self.salary = salary
+        self.pin = pin
+
+
+p = programmer("Tirth", 1200000, 245001)
+print(p.name, p.salary, p.pin, p.company)
+
+r = programmer("Rohan", 1200000, 245001)
+print(r.name, r.salary, r.pin, r.company)
