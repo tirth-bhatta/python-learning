b821bc2cf4a54e69944937f9306977f2ef461eb7
Tirth-Bhatta <tirthbhatta0@gmail.com>
2026-06-24 10:33:31 +0545

    Demonstrate class vs instance attribute precedence in Python

---
+++ CHAPTER10PS/03_problem3.py
@@ -0,0 +1,9 @@
+class Demo:
+    a = 4
+
+
+o = Demo()
+print(o.a)  # prints the class attribute because instance attribute is not present
+o.a = 0  # Instance attribute is set
+print(o.a)  # prints the instance attribute because instance attribute is present
+print(Demo.a) # printd the class attribute
