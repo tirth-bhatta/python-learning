f8c8c9f5a9cecdaefcfe15b3128fcdf5bbf9d59c
Tirth-Bhatta <tirthbhatta0@gmail.com>
2026-06-24 10:27:33 +0545

    Implement basic math operations using OOP in Calculator class

---
+++ CHAPTER10PS/02_problem2.py
@@ -0,0 +1,15 @@
+class Calculator:
+    def __init__(self, n):
+        self.n = n
+
+    def square(self):
+        print(f"The square is {self.n*self.n}")
+    def cube(self):
+        print(f"The cube is {self.n*self.n*self.n}")
+    def squareroot(self):
+        print(f"The squareroot is {self.n ** 0.5}")
+
+a = Calculator(64)
+a.square()
+a.cube()
+a.squareroot()
