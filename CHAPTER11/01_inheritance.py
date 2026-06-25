1b05523f612f2ea648a4636117b775e354ff5c48
Tirth-Bhatta <tirthbhatta0@gmail.com>
2026-06-25 09:39:43 +0545

    Implement inheritance between Employee and Programmer classes

---
+++ CHAPTER11/01_inheritance.py
@@ -0,0 +1,21 @@
+class Employee:
+    company ="ITC"
+    def show(self):
+        print(f"The name is{self.name} and the salary is {self.salary}")
+
+# class Programmer:
+#     company ="ITC Infotech"
+#     def show(self):
+#         print(f"The name is{self.name} and the salary is {self.salary}")
+
+#     def showLanguage(self):
+#         print(f"The name is{self.name} and he is good with {self.language}")
+
+class Programmer(Employee):
+    company = "ITC Infotech"
+    def showLanguage(self):
+        print(f"The name is{self.name} and he is good with {self.language}")
+a = Employee()
+b = Programmer()
+
+print(a.company, b.company)
