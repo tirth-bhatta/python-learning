62407ed31e1223fe66b51590a6419d441b059f5a
Tirth-Bhatta <tirthbhatta0@gmail.com>
2026-06-25 10:04:15 +0545

    Implement constructors in multilevel inheritance

---
+++ CHAPTER11/04_super.py
@@ -0,0 +1,25 @@
+class Employee:
+    def __init__(self):
+        print("Constructor of Employee")
+    a = 1
+
+class Programmer(Employee):
+    def __init__(self):
+        print("Constructor of Programmer")
+    b = 2
+
+class Manager(Programmer):
+    def __init__(self):
+        super().__init__()
+        print("Constructor of Manager")
+    c = 3
+
+o = Employee()
+print(o.a) # Prints the a attribute
+# print(o.b) # shows an error as there is no  b attribute in Employee class
+
+o = Programmer()
+print(o.a, o.b)
+
+o = Manager()
+print(o.a, o.b, o.c)
