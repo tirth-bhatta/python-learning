22179e1e7198a87f0aca983615d4eed4e5ac1921
Tirth-Bhatta <tirthbhatta0@gmail.com>
2026-06-26 15:57:54 +0545

    Add example demonstrating Python match-case statement

---
+++ CHAPTER12/03_match_case.py
@@ -0,0 +1,12 @@
+def http_status(status):
+ match status:
+    case 200:
+     return "OK"
+    case 404:
+     return "Not Found"
+    case 500:
+     return "Internal Server Error"
+    case _:
+     return "Unknown status"
+print(http_status(200))
+print(http_status(404))
