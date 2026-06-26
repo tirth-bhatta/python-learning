feac28b6b33edc38162f419c87a5271f2b888f61
Tirth-Bhatta <tirthbhatta0@gmail.com>
2026-06-26 16:25:18 +0545

    Add example demonstrating try-except-finally behavior

---
+++ CHAPTER12/07_try_finally.py
@@ -0,0 +1,15 @@
+def main():
+    try:
+        a = int(input("Hey: Enter a number: "))
+        print(a)
+        return
+
+    except Exception as e:
+        print(e)
+        return
+
+    finally:
+        print("I am inside finally")
+
+main()
+print("Thank you!")
