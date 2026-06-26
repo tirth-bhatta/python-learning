6a1e934117cb46d82bdcf90487c6d1d3c54fe903
Tirth-Bhatta <tirthbhatta0@gmail.com>
2026-06-26 15:51:07 +0545

    Add examples demonstrating Python type hints

---
+++ CHAPTER12/02_types.py
@@ -0,0 +1,21 @@
+from typing import List, Tuple, Dict, Union
+
+
+# List of integers
+numbers: List[int] = [1, 2, 3, 4, 5]
+# Tuple of a string and an integer
+person: Tuple[str, int] = ("Alice", 30)
+# Dictionary with string keys and integer values
+scores: Dict[str, int] = {"Alice": 90, "Bob": 85}
+# Union type
+identifier: Union[int, str] = "ID123"
+
+n : int = 5
+
+name :str = "tirth"
+
+
+def dum(a : int, b: int) -> int:
+    return a+b
+
+sum(3)
