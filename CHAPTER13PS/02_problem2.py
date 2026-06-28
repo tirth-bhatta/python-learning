b7fde293ed1d6662db70e1972372beee17ac5610
Tirth-Bhatta <tirthbhatta0@gmail.com>
2026-06-28 10:30:54 +0545

    Add formatted student information example

---
+++ CHAPTER13PS/02_problem2.py
@@ -0,0 +1,7 @@
+name = input("enter name: ")
+marks = int(input("Enter marks: "))
+phone = int(input("Phone number: "))
+
+s = "The name of teh student is {}, his marks are {}, and phone number is {}" .format(name, marks, phone)
+
+print(s)
